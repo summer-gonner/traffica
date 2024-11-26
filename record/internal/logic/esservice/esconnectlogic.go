d08e580c58c6993db745db8d8f953e508487a563
shenyuan <2497822530@qq.com>
2024-11-26 18:19:42 +0800

    es11

--- record/internal/logic/esservice/esconnectlogic.go
+++ record/internal/logic/esservice/esconnectlogic.go
@@ -2,11 +2,9 @@ package esservicelogic
 
 import (
 	"context"
-	"github.com/summer-gonner/traffica/pkg/es"
 	"github.com/summer-gonner/traffica/record/internal/svc"
 	"github.com/summer-gonner/traffica/record/recordclient"
 	"github.com/zeromicro/go-zero/core/logx"
-	"google.golang.org/appengine/log"
 )
 
 type EsConnectLogic struct {
@@ -25,16 +23,16 @@ func NewEsConnectLogic(ctx context.Context, svcCtx *svc.ServiceContext) *EsConne
 
 func (l *EsConnectLogic) EsConnect(in *recordclient.EsReq) (*recordclient.EsResp, error) {
 
-	client := es.Client{
-		Address:  in.Address,
-		Password: in.Password,
-		Username: in.Username,
-	}
-	err := client.Connect()
-	if err != nil {
-		log.Infof(l.ctx, "es连接失败%v", err)
-		return nil, err
-	}
+	//client := es.Client{
+	//	Address:  in.Address,
+	//	Password: in.Password,
+	//	Username: in.Username,
+	//}
+	//err := client.Connect()
+	//if err != nil {
+	//	log.Infof(l.ctx, "es连接失败%v", err)
+	//	return nil, err
+	//}
 	logx.Infof("es连接成功")
 	return &recordclient.EsResp{
 		Result:  true,
