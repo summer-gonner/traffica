d1072874716eaf1f20d0c60ae19c76f4a08bb924
shenyuan <2497822530@qq.com>
2024-11-26 18:39:40 +0800

    5

--- record/internal/logic/esservice/esconnectlogic.go
+++ record/internal/logic/esservice/esconnectlogic.go
@@ -2,6 +2,7 @@ package esservicelogic
 
 import (
 	"context"
+	"fmt"
 	"github.com/summer-gonner/traffica/pkg/es"
 	"github.com/summer-gonner/traffica/record/internal/svc"
 	"github.com/summer-gonner/traffica/record/recordclient"
@@ -23,11 +24,24 @@ func NewEsConnectLogic(ctx context.Context, svcCtx *svc.ServiceContext) *EsConne
 }
 
 func (l *EsConnectLogic) EsConnect(in *recordclient.EsReq) (*recordclient.EsResp, error) {
+	if in == nil {
+		return nil, fmt.Errorf("EsReq is nil")
+	}
+
+	if in.Address == "" {
+		return nil, fmt.Errorf("elasticsearch address is empty")
+	}
+	if in.Username == "" {
+		return nil, fmt.Errorf("elasticsearch username is empty")
+	}
+	if in.Password == "" {
+		return nil, fmt.Errorf("elasticsearch password is empty")
+	}
 
 	client := &es.Client{
-		Address:  "http://47.101.198.49:9200", // 设置为你的 Elasticsearch 地址
-		Username: "",
-		Password: "",
+		Address:  in.Address, // 设置为你的 Elasticsearch 地址
+		Username: in.Username,
+		Password: in.Password,
 	}
 
 	// 连接到 Elasticsearch
